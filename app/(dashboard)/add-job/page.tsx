658b295cea1c43b9317bd377c14d163a6175e7eb
Sharo <redshanks@Sharos-MacBook-Pro.local>
2024-04-03 20:28:47 -0400

    first commit

---
+++ app/(dashboard)/add-job/page.tsx
@@ -0,0 +1,21 @@
+import CreateJobForm from '@/components/CreateJobForm'
+import {
+  dehydrate,
+  HydrationBoundary,
+  QueryClient,
+} from '@tanstack/react-query';
+
+const AddJobPage = () => {
+  const queryClient = new QueryClient()
+  return (
+        
+    <HydrationBoundary state={dehydrate(queryClient)}>
+        <CreateJobForm />
+    </HydrationBoundary>
+
+
+
+  )
+}
+
+export default AddJobPage
---
+++ app/page.tsx
@@ -0,0 +1,51 @@
+import { Button } from "@/components/ui/button";
+import { Camera} from 'lucide-react'
+import Image from "next/image";
+import Logo from "../assets/logo.svg"
+import LandingImg from "../assets/main.svg"
+import Link from "next/link";
+
+export default function Home() {
+  return (
+    <main>
+
+      <header className="max-w-6xl mx-auto px-4 sm:px-8 py-6">
+
+        <Image 
+          src={Logo}
+          alt="Logo"
+        />
+
+        <section className="max-w-6xl mx-auto px-4 sm:px-8 h-screen -mt-20 grid lg:grid-cols-[1fr,400px] items-center">
+
+          <div>
+              <h1 className="capitalize text-4xl md:text-7xl font-bold">
+                job <span className="text-primary">tracking</span>app
+              </h1>
+              <p className="leading-loose max-w-md mt-4">
+                Lorem ipdumks aksndfj andfo iadsfsad lasdjf
+              </p>
+
+              <Button asChild className="mt-4">
+                <Link href="/add-job">
+                  Get Started
+                </Link>
+
+              </Button>
+          </div>
+
+          <Image
+          src={LandingImg}
+          alt="landing"
+          className="hidden lg:block"
+           />
+
+
+
+        </section>
+                  
+      </header>
+
+    </main>
+  )
+}
